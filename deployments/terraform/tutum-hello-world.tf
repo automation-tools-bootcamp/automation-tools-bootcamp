f8495dfa0f0366ff6e62336bf4bbe42f8ff7149d
Tyler Fitch <tfitch@getchef.com>
2016-03-10 18:16:22 -0800

    Created alternate Terraform template using Tutum haproxy example.

---
+++ deployments/terraform/tutum-hello-world.tf
@@ -0,0 +1,32 @@
+# how many copies of the container we will start
+variable "count" {
+  default = 3
+}
+
+# define what container we are going to use
+# and pull it from our Artifactory Docker registry (localhost:5002)
+resource "docker_image" "hello-world" {
+  name = "tutum/hello-world:latest"
+}
+
+# Create the containers
+resource "docker_container" "hello-world" {
+  image = "${docker_image.hello-world.latest}"
+  // using the count var automatically invokes this multiple times, no "loop" required
+  count = "${var.count}"
+  name = "hello-world-${count.index}"
+}
+
+# and now front it all with a haproxy container
+resource "docker_image" "haproxy" {
+  name = "tutum/haproxy:latest"
+}
+
+resource "docker_container" "haproxy" {
+  depends_on = ["docker_container.hello-world"]
+  image = "${docker_image.haproxy.latest}"
+  ports = {external = 8088
+           internal = 80}
+  links = ["${docker_container.hello-world.*.name}"]
+  name = "haproxy"
+}
