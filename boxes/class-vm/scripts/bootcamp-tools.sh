f7f264f79c6f78ae59a5f8be4bc12f80145a852b
Tyler Fitch <tfitch@chef.io>
2016-12-03 12:14:04 -0800

    The Packer scripts to create the VM for the LISA 2016 class.

--- boxes/class-vm/scripts/bootcamp-tools.sh
+++ boxes/class-vm/scripts/bootcamp-tools.sh
@@ -7,9 +7,10 @@ set -e
 apt-get install -y linux-image-extra-$(uname -r)
 apt-get install -y apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | apt-key add -
 
 echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' | tee /etc/apt/sources.list.d/docker.list
+echo 'deb http://download.virtualbox.org/virtualbox/debian trusty contrib' >> /etc/apt/sources.list
 
 apt-get update
 apt-cache policy docker-engine
@@ -24,17 +25,30 @@ wget -O /home/vagrant/jenkins/jenkins.war http://mirrors.jenkins.io/war-stable/l
 chown -R vagrant:vagrant /home/vagrant/jenkins
 
 # install Terraform
-wget https://releases.hashicorp.com/terraform/0.6.12/terraform_0.6.12_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.7.13/terraform_0.7.13_linux_amd64.zip
 mkdir -p /usr/local/terraform/bin
-unzip terraform_0.6.12_linux_amd64.zip -d /usr/local/terraform/bin
+unzip terraform_0.7.13_linux_amd64.zip -d /usr/local/terraform/bin
+
+# install Packer
+wget https://releases.hashicorp.com/packer/0.12.0/packer_0.12.0_linux_amd64.zip
+mkdir -p /usr/local/packer/bin
+unzip packer_0.12.0_linux_amd64.zip -d /usr/local/packer/bin
 
 # setup steps for Artifactory and Jenkins
 mkdir -p /var/opt/jfrog/artifactory
 mkdir -p /home/vagrant/jenkins_home
 chown vagrant:vagrant /home/vagrant/jenkins_home
 
-# install the ChefDK for InSpec
-curl https://omnitruck.chef.io/install.sh | bash -s -- -c current -P chefdk
+# install the ChefDK for Chef and InSpec
+curl https://omnitruck.chef.io/install.sh | bash -s -- -c stable -P chefdk
+
+
+# as well as Virtualbox and Vagrant because it is going to get weird aka VM inside the VM
+# wget http://download.virtualbox.org/virtualbox/5.1.10/virtualbox-5.1_5.1.10-112026~Ubuntu~trusty_amd64.deb
+# dpkg -i virtualbox-5.1_5.1.10-112026~Ubuntu~trusty_amd64.deb
+apt-get install -y virtualbox-5.1
+wget https://releases.hashicorp.com/vagrant/1.9.0/vagrant_1.9.0_x86_64.deb
+dpkg -i vagrant_1.9.0_x86_64.deb
 
 # install Node.js because it's nice to have around
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
@@ -42,8 +56,8 @@ apt-get install -y nodejs
 
 # custom bash setup
 echo "export ARTIFACTORY_HOME=/var/opt/jfrog/artifactory" >> /etc/environment
-echo "PATH=/usr/local/terraform/bin:$PATH" >> /etc/environment
 echo 'eval "$(chef shell-init bash)"' >> /etc/environment
+echo "PATH=/home/vagrant/.chefdk/gem/ruby/2.3.0/bin:/usr/local/packer/bin:/usr/local/terraform/bin:$PATH" >> /etc/environment
 
 # install tmux - with screen conf file
 apt-get install -y tmux
--- boxes/class-vm/scripts/docker-pulls.sh
+++ boxes/class-vm/scripts/docker-pulls.sh
@@ -4,8 +4,8 @@
 set -e
 
 # prebake the Docker containers
-docker pull jenkins:1.625.2
 docker pull jfrog-docker-reg2.bintray.io/jfrog/artifactory-registry
 docker pull node:latest
+docker pull node:0.10
 docker pull tutum/hello-world:latest
 docker pull tutum/haproxy:latest
