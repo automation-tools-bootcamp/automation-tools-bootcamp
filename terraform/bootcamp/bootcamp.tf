bc3b93e14ba6aca66a18314622b1ef92a788cb52
Bert Cosemans <bert@cosemans.be>
2016-12-04 15:34:30 -0500

    Fix bootcamp terraform file

--- terraform/bootcamp/bootcamp.tf
+++ terraform/bootcamp/bootcamp.tf
@@ -23,10 +23,10 @@ resource "docker_image" "haproxy" {
 }
 
 resource "docker_container" "haproxy" {
-  depends_on = ["docker_container.hello-world"]
+  depends_on = ["docker_container.atb"]
   image = "${docker_image.haproxy.latest}"
   ports = {external = 8086
-           internal = 8085}
+           internal = 80}
   links = ["${docker_container.atb.*.name}"]
   name = "haproxy"
 }
